3f9f608ef5f5a567bb6e3a81f70f3429493de327
Luis Zayas <zayaslui@gmail.com>
2017-09-02 18:43:25 -0400

    update

--- prueba.php
+++ prueba.php
@@ -6,17 +6,27 @@ $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("MYSQL_USER");
 $dbpwd = getenv("MYSQL_PASSWORD");
 $dbname = getenv("MYSQL_DATABASE");
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
-if ($connection->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-} else {
-    printf("Connected to the database");
-}
-$connection->close();
 
-echo "hola que tal luis,.....asdjflkjasdkjfklajsd.";
 
-var_dump(getenv());
+echo $dbhost;
+echo $dbport;
+echo $dbuser;
+echo $dbpwd;
+echo $dbname;
+
+
+
+// $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+// if ($connection->connect_errno) {
+//     printf("Connect failed: %s\n", $mysqli->connect_error);
+//     exit();
+// } else {
+//     printf("Connected to the database");
+// }
+// $connection->close();
+
+// echo "hola que tal luis,.....asdjflkjasdkjfklajsd.";
+
+// var_dump(getenv());
 
 ?>
