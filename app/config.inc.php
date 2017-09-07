11b4eeccb73938444dcb7ecd90159f6020af9e52
Luis Zayas <zayaslui@gmail.com>
2017-09-07 18:07:04 -0400

    update

--- app/config.inc.php
+++ app/config.inc.php
@@ -19,4 +19,21 @@
 		$config['nombre_base_datos'] = 'turismo';
 	}	
 
+	echo "probando";
+	
+	$dbhost = getenv("MYSQL_SERVICE_HOST");
+	$dbport = getenv("MYSQL_SERVICE_PORT");
+	$dbuser = getenv("MYSQL_USER");
+	$dbpwd = getenv("MYSQL_PASSWORD");
+	$dbname = getenv("MYSQL_DATABASE");
+	$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+	if ($connection->connect_errno) {
+	    printf("Connect failed: %s\n", $mysqli->connect_error);
+	    exit();
+	} else {
+	    printf("Connected to the database");
+	}
+	$connection->close();
+
+
 ?>
