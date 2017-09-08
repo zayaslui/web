0dce2d791d476f52104ae7b66005d16754e20018
Luis Zayas <zayaslui@gmail.com>
2017-09-08 14:18:12 -0400

    udpate error

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <?php
-	error_reporting(E_ALL & ~(E_STRICT|E_NOTICE));
+	error_reporting(E_ALL ^ E_NOTICE);
+
 	include_once "app/Conexion.inc.php";
 	include_once "app/RepositorioEmpresas.inc.php";
 	Conexion::abrir_conexion();
