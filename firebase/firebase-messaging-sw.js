df9674187c37360007db297fa6f3897234e7e071
Luis Zayas <zayaslui@gmail.com>
2017-10-04 17:55:00 -0300

    udpate firebase

---
+++ firebase/firebase-messaging-sw.js
@@ -0,0 +1,5 @@
+importScripts('https://prueba-1852.firebaseapp.com/__/firebase/3.8.0/firebase-app.js');
+importScripts('https://prueba-1852.firebaseapp.com/__/firebase/3.8.0/firebase-messaging.js');
+importScripts('https://prueba-1852.firebaseapp.com/__/firebase/init.js');
+
+firebase.messaging();
